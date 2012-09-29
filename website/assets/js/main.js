ebcc28e527c51a3926cb944197841503a97ec444
Davidson Fellipe <email@fellipe.com>
2012-09-29 01:56:19 -0300

    melhorando a organizacao do LESS

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -4,13 +4,11 @@
         + "&per_page=100"
         + "&page=1";
 
-    var TEMPLATE = '<div class="span4">' +
+    var TEMPLATE = '<div class="span4 project">' +
 			       '	<div class="container-inner">' +
 				   '      <h2>#{title}</h2>' +
-				   '       <p>#{description}</p>' +
-				   '      <p>' +
-				   '      	<a class="btn" href="#{html_url}">View details &raquo;</a>' +
-				   '      </p>' +
+				   '      <p class="description">#{description}</p>' +
+				   '      <a class="btn" href="#{html_url}">View details &raquo;</a>' +
 			       '  	</div>' +
 			       '	<div class="countdown-break"></div>' +
 			       '</div>';
@@ -24,10 +22,9 @@
 	  	for ( var i = 0; i < result.data.length ; i++ ) {
 
 	  		repos += TEMPLATE.replace("#{title}", result.data[i].name)
-                         .replace("#{description}", result.data[i].description)
-	  						         .replace("#{html_url}", result.data[i].html_url);
+                       		 .replace("#{description}", result.data[i].description)
+	  						 .replace("#{html_url}", result.data[i].html_url);
 
-			// repos += ( i % 3 === 0 && i > 1 ) ? '<br class="clear">' : '';
 	  	}
 
 	  	$(".repos").append(repos);
