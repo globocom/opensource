6a0cf7e011dca2903f23e2c1003262e038d8b192
Francisco Souza <f@souza.cc>
2015-02-05 09:56:25 -0200

    js/main: não exibindo IWantToWorkAtGloboCom na lista de projetos

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -1,8 +1,6 @@
 (function ($, undefined) {
-
     var URL = "https://api.github.com/orgs/#{org}/repos?callback=?&per_page=100&page=1";
     var organizations = ["globocom", "clappr", "thumbor", "tsuru"];
-
     var PROJECT = '<div class="span4 project hide">' +
                    '    <div class="container-inner">' +
                    '      <h2>#{title}</h2>' +
@@ -11,23 +9,18 @@
                    '    </div>' +
                    '    <div class="countdown-break"></div>' +
                    '</div>';
-
     var MEMBER = '<li class="span2 member hide">' +
                    '    <img class="photo" src="#{url}" width="50" height="50">' +
                    '    <a class="url" href="#{url}">@#{login}</a>' +
                    '</li>';
-
     var AJAX = [];
 
-    for ( var i = 0; i < organizations.length ; i++ ) {
-
+    for (var i = 0; i < organizations.length; i++) {
       var uri = URL.replace("#{org}", organizations[i]);
       AJAX.push($.getJSON(uri));
-
     }
 
-    $.when.apply( $, AJAX ).done( function() {
-
+    $.when.apply($, AJAX).done( function() {
       var repos = [];
       var rendered_repos = "";
 
@@ -41,38 +34,32 @@
         (a.stargazers_count > b.stargazers_count) ? -1 : 0; });
 
       for ( i = 0; i < repos.length; i++ ) {
-
-        rendered_repos += PROJECT.replace("#{title}", repos[i].name)
-                        .replace("#{description}", repos[i].description)
-                        .replace("#{html_url}", repos[i].html_url);
-
+        if (repos[i] !== "IWantToWorkAtGloboCom") {
+          rendered_repos += PROJECT.replace("#{title}", repos[i].name)
+                          .replace("#{description}", repos[i].description)
+                          .replace("#{html_url}", repos[i].html_url);
+        }
       }
 
       $(".repos").empty();
       $(".repos").append(rendered_repos);
       $(".project").fadeIn(1000);
-
     });
 
-    $.getJSON("https://api.github.com/orgs/globocom/public_members?page=1&per_page=100&callback=?", function (result) {
+    $.getJSON("https://api.github.com/orgs/globocom/public_members?page=1&per_page=100&callback=?", function(result) {
         if (result.data && result.data.length > 0) {
-
             var members = "", item = "";
 
-            for ( var i = 0; i < result.data.length ; i++ ) {
-
+            for (var i = 0; i < result.data.length; i++) {
                 members += MEMBER.replace("#{url}", result.data[i].avatar_url)
                                   .replace("#{login}", result.data[i].login)
                                   .replace("#{url}", result.data[i].url
                                   .replace("api.", "")
                                   .replace("users/", ""));
-
             }
 
             $(".members").empty().append(members);
             $(".member").fadeIn(1000);
           }
-
     });
-
 })(jQuery);
