bf948880e87939fff132a39387cff25ebc7184fa
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-20 02:21:51 -0300

    all: customize some gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Globo.com - Por que ❤️ Open Source?',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Globo.com - Open Source',
+        short_name: 'Globo.com Open Source',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/app-icon.png',
       },
     },
-    'gatsby-plugin-offline',
   ],
 }
