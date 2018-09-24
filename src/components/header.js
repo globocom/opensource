5a6d14c7ee1e700ad60b61fb2734f0264f37d16f
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-23 22:32:43 -0300

    Add active menu links

--- src/components/header.js
+++ src/components/header.js
@@ -4,22 +4,38 @@ import { Link } from 'gatsby'
 import logo from '../images/logo.svg'
 import styles from './header.module.css'
 
-const Header = () => (
+const isMenuItemActive = ({ isCurrent }) => {
+  const classNames = [styles.menuLink]
+  if (isCurrent) {
+    classNames.push(styles.menuLinkActive)
+  }
+  return { className: classNames.join(' ') }
+}
+
+const Header = props => (
   <header className={styles.header}>
     <div className={styles.brand}>
-      <img className={styles.logo} src={logo} alt="Globo.com" />
+      <Link to="/">
+        <img className={styles.logo} src={logo} alt="Globo.com" />
+      </Link>
       <span className={styles.brandName}>Open Source</span>
     </div>
     <nav>
       <ul className={styles.menu}>
-        <li className={styles.menuItemActive}>
-          <Link to="/projetos/">Projetos</Link>
+        <li className={styles.menuItem}>
+          <Link getProps={isMenuItemActive} to="/projetos/">
+            Projetos
+          </Link>
         </li>
         <li className={styles.menuItem}>
-          <Link to="/nosso-time/">Nosso time</Link>
+          <Link getProps={isMenuItemActive} to="/nosso-time/">
+            Nosso time
+          </Link>
         </li>
         <li className={styles.menuItem}>
-          <Link to="/hacktober/">Hacktober</Link>
+          <Link getProps={isMenuItemActive} to="/hacktober/">
+            Hacktober
+          </Link>
         </li>
       </ul>
     </nav>
