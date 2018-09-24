f0cef8c5cd96b4b991eaf953cc28faf333e2c6c0
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-24 00:26:42 -0300

    Add some responsiveness to the header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -13,33 +13,31 @@ const isMenuItemActive = ({ isCurrent }) => {
 }
 
 const Header = () => (
-  <header className={styles.header}>
+  <header className={styles.header} role="navigation">
     <div className={styles.headerContent}>
       <div className={styles.brand}>
-        <Link to="/">
-          <img className={styles.logo} src={logo} alt="Globo.com" />
+        <Link className={styles.brandLink} to="/">
+          <img className={styles.brandLogo} src={logo} alt="Globo.com" />
+          <span className={styles.brandName}>Open Source</span>
         </Link>
-        <span className={styles.brandName}>Open Source</span>
       </div>
-      <nav>
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}>
-            <Link getProps={isMenuItemActive} to="/projetos/">
-              Projetos
-            </Link>
-          </li>
-          <li className={styles.menuItem}>
-            <Link getProps={isMenuItemActive} to="/nosso-time/">
-              Nosso time
-            </Link>
-          </li>
-          <li className={styles.menuItem}>
-            <Link getProps={isMenuItemActive} to="/hacktober/">
-              Hacktober
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          <Link getProps={isMenuItemActive} to="/projetos/">
+            Projetos
+          </Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link getProps={isMenuItemActive} to="/nosso-time/">
+            Nosso time
+          </Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link getProps={isMenuItemActive} to="/hacktober/">
+            Hacktober
+          </Link>
+        </li>
+      </ul>
     </div>
   </header>
 )
