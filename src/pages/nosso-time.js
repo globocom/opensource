e08187fb338773dd5e13c695ca5d6335949304b9
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-23 22:34:16 -0300

    Create new blank pages

---
+++ src/pages/nosso-time.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const NossoTimePage = () => (
+  <Layout>
+    <h1>Nosso time</h1>
+  </Layout>
+)
+
+export default NossoTimePage
