e8db4e8f3af02d6b6530f66021f41e25a2585fa8
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-23 22:34:16 -0300

    Create new blank pages

---
+++ src/pages/projetos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ProjetosPage = () => (
+  <Layout>
+    <h1>Projetos</h1>
+  </Layout>
+)
+
+export default ProjetosPage
