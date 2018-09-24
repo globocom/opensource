6ec81395a524cb4e5e76a823c2f32ac73971e32e
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-24 01:53:19 -0300

    Create the landingpage main section

---
+++ src/data/featuredProjects.js
@@ -0,0 +1,34 @@
+import megadraftLogo from '../images/logos/logo_megadraft.svg'
+import tsuruLogo from '../images/logos/logo_tsuru.svg'
+import thumborLogo from '../images/logos/logo_thumbor.svg'
+import clapprLogo from '../images/logos/logo_clappr.svg'
+
+export default [
+  {
+    name: 'Megradraft',
+    slug: 'megadraft',
+    thumb: megadraftLogo,
+    description:
+      'Megradraft is a Rich Text editor built on top of Facebook’s Draft.JS featuring a nice default base of components and extensibility.',
+  },
+  {
+    name: 'Tsuru',
+    slug: 'tsuru',
+    thumb: tsuruLogo,
+    description:
+      'Open source, extensible and Doker-based Platform as a Service (PaaS).',
+  },
+  {
+    name: 'Thumbor',
+    slug: 'thumbor',
+    thumb: thumborLogo,
+    description:
+      'Thumbor is an open-source photo thumbnail service by globo.com.',
+  },
+  {
+    name: 'Clappr',
+    slug: 'clappr',
+    thumb: clapprLogo,
+    description: 'Clappr is an extensible media player for the web.',
+  },
+]
