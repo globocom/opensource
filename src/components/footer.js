3adfc18355534b03cad9fe347b1ebf7084486832
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-24 00:26:42 -0300

    Add some responsiveness to the header and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import styles from './footer.module.css'
 
@@ -6,13 +7,15 @@ const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.footerContent}>
       <div className={styles.navigation}>
-        <span className={styles.brand}>Open source</span>
+        <Link to="/" className={styles.brand}>
+          Open source
+        </Link>
         <div className={styles.menu}>
           <a
             className={styles.menuItem}
             target="_blank"
             rel="noopener noreferrer"
-            href="https://github.com/globocom/"
+            href="https://github.com/globocom"
           >
             GITHUB
           </a>
