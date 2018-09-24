89337517c48696edb13384546f8fae895dc40787
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-24 14:57:10 -0300

    Add a section to the body

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout'
 import FeaturedProjects from '../components/featuredProjects'
+import Button from '../components/button'
 import featuredProjects from '../data/featuredProjects'
 
 import styles from './index.module.css'
+import githubIcon from '../images/logo-github.svg'
 
 const IndexPage = () => (
   <Layout>
@@ -11,7 +13,10 @@ const IndexPage = () => (
       <div>
         <h1 className={styles.heroSectionCall}>Addicted Developers</h1>
         <h2 className={styles.heroSectionSubCall}>Unidos pelo código.</h2>
-        <a className={styles.heroSectionLink} href="">
+        <a
+          className={styles.heroSectionLink}
+          href="https://github.com/globocom/opensource"
+        >
           Let's code
         </a>
       </div>
@@ -20,6 +25,30 @@ const IndexPage = () => (
       <div className={styles.sectionContent}>
         <h2 className={styles.sectionTitle}>Nossos projetos</h2>
         <FeaturedProjects projects={featuredProjects} />
+        <Button
+          url="https://github.com/globocom/opensource"
+          className={styles.sectionButton}
+          label="ver todos"
+          icon={githubIcon}
+        />
+      </div>
+    </section>
+    <section className={styles.sectionBottom}>
+      <div className={styles.sectionBottomContent}>
+        <div className={styles.sectionBottomData}>
+          <div className={styles.sectionBottomTitle}>Trabalhe conosco</div>
+          <div className={styles.sectionBottomSubTitle}>
+            Você nasceu pra isso!
+          </div>
+          <div className={styles.sectionBottomLink}>
+            <Button
+              url="https://github.com/globocom/opensource"
+              className={styles.sectionButton}
+              label="participe"
+              transparent={true}
+            />
+          </div>
+        </div>
       </div>
     </section>
   </Layout>
