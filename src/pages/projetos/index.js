5c8e37785bfd909654647bde80fe67bbaf7543da
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-09-30 16:09:09 -0300

    Add projects page a bgtop

--- src/pages/projetos/index.js
+++ src/pages/projetos/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../components/layout'
+import TopBackground from '../../components/topBackground'
 
 import styles from '../../components/layout.module.css'
 
 const ProjetosPage = () => (
-  <Layout>
+  <Layout renderTop={() => <TopBackground skyObject="rocket" />}>
     <section className={styles.mainSection}>
       <h1>Projetos</h1>
     </section>
