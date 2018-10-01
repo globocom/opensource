97c1626fc02335c4607fca6d9685fe5ce4f21816
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-10-01 11:12:31 -0300

    Create a github service

--- src/pages/nosso-time.js
+++ src/pages/nosso-time.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Layout from '../components/layout'
+import TopBackground from '../components/topBackground'
+
+// import gql from 'graphql-tag'
+// import { ApolloProvider } from 'react-apollo'
+// import { Query } from 'react-apollo'
+
+import { getOrganizationMembers } from '../services/github'
 
 import styles from '../components/layout.module.css'
 
-const NossoTimePage = () => (
-  <Layout>
-    <section className={styles.mainSection}>
-      <h1>Nosso time</h1>
-    </section>
-  </Layout>
-)
+class NossoTimePage extends Component {
+  state = {
+    members: [],
+  }
+
+  async componentDidMount() {
+    const data = await getOrganizationMembers()
+    // this.setState({ members: data.organization.members })
+    console.log(members)
+  }
+
+  render() {
+    const { members } = this.state
+    return (
+      <Layout renderTop={() => <TopBackground skyObject="moon" />}>
+        <h1>Nosso Time</h1>
+        {members.map(member => (
+          <span>{member.name}</span>
+        ))}
+      </Layout>
+    )
+  }
+}
 
 export default NossoTimePage
