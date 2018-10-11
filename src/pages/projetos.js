4b719c6ffa2e904ad736fbfd64b856997d3a5036
Thiago Rodrigues Magalhães <thiagoelmagalhaes@gmail.com>
2018-10-11 15:26:35 -0300

    ver mais in new tab (#20)

--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -23,7 +23,7 @@ const Repo = ({ repo }) => (
       issues={repo.issues.totalCount}
     />
     <div className={styles.projectDescription}>{repo.description}</div>
-    <a className={styles.projectLink} href={repo.url}>
+    <a className={styles.projectLink} href={repo.url} target="_blank" rel="noopenner noreferrer">
       ver detalhes
     </a>
   </div>
