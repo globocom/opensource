e5166e7512db76f684155b30ba9fbd66392b3d86
Artur Sousa <arturfelipe.sousa@gmail.com>
2018-10-30 12:58:07 -0300

    Add form labels

--- src/components/address-dialog.js
+++ src/components/address-dialog.js
@@ -3,6 +3,21 @@ import Button from './button'
 
 import styles from './address-dialog.module.css'
 
+const Field = ({ label, type = 'text', placeholder, value, onChange }) => {
+  return (
+    <div className={styles.field}>
+      {label && <label className={styles.fieldLabel}>{label}</label>}
+      <input
+        className={styles.textField}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
+
 class AddressDialog extends Component {
   state = {
     name: '',
@@ -21,12 +36,13 @@ class AddressDialog extends Component {
     const address = {
       ...this.state,
     }
+
     console.log(address)
   }
 
   render() {
     const { open = false, onClose } = this.props
-    if (!open) return null
+    if (open) return null
 
     return (
       <div className={styles.dialog}>
@@ -36,60 +52,45 @@ class AddressDialog extends Component {
             <div className={styles.head}>Endereço</div>
             <div className={styles.form}>
               <p>Preencha com o endereço de envio da camiseta.</p>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
-                  type="text"
-                  placeholder="Nome"
+              <form>
+                <Field
+                  label="Nome"
+                  placeholder="Seu nome completo"
                   value={this.state.name}
                   onChange={this.handleFieldChange('name')}
                 />
-              </div>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
+                <Field
                   type="email"
-                  placeholder="E-mail"
+                  label="E-mail"
+                  placeholder="voce@example.com"
                   value={this.state.email}
                   onChange={this.handleFieldChange('email')}
                 />
-              </div>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
-                  type="text"
-                  placeholder="Estado"
+                <Field
+                  label="Estado"
+                  placeholder="RJ"
                   value={this.state.state}
                   onChange={this.handleFieldChange('state')}
                 />
-              </div>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
-                  type="text"
-                  placeholder="Cidade"
+                <Field
+                  label="Cidade"
+                  placeholder="Rio de Janeiro"
                   value={this.state.city}
                   onChange={this.handleFieldChange('city')}
                 />
-              </div>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
-                  type="text"
-                  placeholder="Endereço"
+                <Field
+                  label="Endereço"
+                  placeholder="Avenida das américas 700 - Bloco 2 - Sala 3"
                   value={this.state.address}
                   onChange={this.handleFieldChange('address')}
                 />
-              </div>
-              <div className={styles.field}>
-                <input
-                  className={styles.textField}
-                  type="text"
-                  placeholder="CEP"
-                  value={this.state.postalcode}
+                <Field
+                  label="CEP"
+                  placeholder="00000-000"
+                  value={this.state.address}
                   onChange={this.handleFieldChange('postalcode')}
                 />
-              </div>
+              </form>
             </div>
             <div className={styles.footer}>
               <Button
