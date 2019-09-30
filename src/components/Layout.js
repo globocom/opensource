47ae88407ebc6afbe3794d4e0952a42a78ea3294
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-09-30 00:09:31 -0300

    Create the HomeTop

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 const Main = styled.main`
-  padding: 2rem 0;
+  padding: ${props => (props.noPadding ? "0" : "2rem 0")};
   min-height: calc(100vh - 3.75rem - 7rem);
 
   ${media.greaterThan("large")`
@@ -16,12 +16,12 @@ const Main = styled.main`
   `}
 `
 
-function Layout({ children, darkHeader, darkFooter }) {
+function Layout({ children, darkHeader, darkFooter, noPadding }) {
   return (
     <React.Fragment>
       <GlobalStyle />
       <Header dark={darkHeader} />
-      <Main>{children}</Main>
+      <Main noPadding={noPadding}>{children}</Main>
       <Footer dark={darkFooter} />
     </React.Fragment>
   )
@@ -31,11 +31,13 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
   darkHeader: PropTypes.bool,
   darkFooter: PropTypes.bool,
+  noPadding: PropTypes.bool,
 }
 
 Layout.defaultProps = {
   darkHeader: false,
   darkFooter: false,
+  noPadding: false,
 }
 
 export default Layout
