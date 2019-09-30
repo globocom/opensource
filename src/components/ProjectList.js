79f027d5add67c352edcbd7a05feba9f9665d5c0
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-09-30 03:40:51 -0300

    Show Repository data

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,17 +1,26 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
+import media from "styled-media-query"
 
 import ButtonLink from "./ButtonLink"
 
 const ProjectListWrapper = styled.section`
   display: flex;
   flex-direction: column;
+
+  ${media.greaterThan("medium")`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 40px;
+  `}
 `
 
 const ProjectListButton = styled.div`
-  text-align: center;
   margin: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 function ProjectList({ children, hasShowAll }) {
