861324bf1093cee59ff014b0c04efe21662d8d9d
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-10-01 03:03:27 -0300

    Add a blank prop to the ButtonLink

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -8,9 +8,16 @@ const ButtonLinkWrapper = styled(ButtonWrapper).attrs({
   as: "a",
 })``
 
-function ButtonLink({ children, href, dark }) {
+function ButtonLink({ children, href, dark, blank, anchorProps }) {
+  const elProps = { ...(anchorProps || {}) }
+
+  if (blank) {
+    elProps.target = "_blank"
+    elProps.rel = "noopener noreferrer"
+  }
+
   return (
-    <ButtonLinkWrapper href={href} dark={dark}>
+    <ButtonLinkWrapper href={href} dark={dark} {...elProps}>
       {children}
     </ButtonLinkWrapper>
   )
@@ -20,6 +27,12 @@ ButtonLink.propTypes = {
   children: PropTypes.node.isRequired,
   dark: PropTypes.bool,
   href: PropTypes.string,
+  blank: PropTypes.bool,
+  anchorProps: PropTypes.object,
+}
+
+ButtonLink.defaultProps = {
+  blank: false,
 }
 
 export default ButtonLink
