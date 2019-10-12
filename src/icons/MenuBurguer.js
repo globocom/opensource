a3a16f89abc99d5f7524d340ca4241fadedf52a5
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-10-12 15:50:19 -0300

    Move GloboCom and MenuBurguer icons to the src/icons

---
+++ src/icons/MenuBurguer.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default ({ width = 24, height = 24 }) => (
+  <svg width={width} height={height} viewBox="0 0 24 24">
+    <title>Menu</title>
+    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+  </svg>
+)
