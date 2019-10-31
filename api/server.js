8a121b8b611fd129ad9aa07184960eeff50f6da1
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-10-31 02:01:13 -0300

    Add a /subscribe route to the demo server

--- api/server.js
+++ api/server.js
@@ -8,6 +8,38 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
+// server.get("/user", (req, res, next) => {
+//   if (
+//     !req.headers.cookie ||
+//     req.headers.cookie.indexOf("logged_in=yes") === -1
+//   ) {
+//     res.sendStatus(401)
+//   } else {
+//     next()
+//   }
+// })
+
+server.use(bodyParser.text())
+server.post("/subscribe", (req, res, next) => {
+  const data = JSON.parse(req.body)
+  const db = router.db
+  const user = db
+    .get("user")
+    .cloneDeep()
+    .value()
+
+  user.result = {
+    ...user.result,
+    ...data,
+  }
+
+  db.get("user")
+    .assign(user)
+    .write()
+
+  res.status(201).jsonp(db.get("user").value())
+})
+
 server.use(router)
 server.listen(3000, () => {
   console.log("Demo api server is running...")
